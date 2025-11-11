8857adf84498281e56947db806ae6b175e754997
Jorge Montesinos <osmarmontes50@gmail.com>
2025-11-10 21:43:05 -0500

    avance

---
+++ src/modules/Auth/LoginView.jsx
@@ -0,0 +1,49 @@
+import { useRef } from 'react'
+import useAuthStore from '../../store/useAuthStore';
+
+const LoginView = () => {
+
+  // const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  // const { registerUser } = useAuthStore();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const dataUser = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value
+    }
+    // console.table(dataUser)
+    // await registerUser(dataUser);
+  }
+
+  return (
+    <div className='max-w-7xl mx-auto px-3 md:px-6 py-6 flex flex-col items-center'>
+      <h2 className='text-xl font-bold'>Ingresa</h2>
+      <div className='card bg-base-100 w-96 shadow-sm'>
+        <form onSubmit={handleSubmit}>
+          {/* <div className='mb-3'>
+            <label className='mb-2 block'>Nombre</label>
+            <input ref={nameRef} type="text" className='input w-full'/>
+          </div> */}
+          <div className='mb-3'>
+            <label className='mb-2 block'>Email</label>
+            <input ref={emailRef} type="email" className='input w-full'/>
+          </div>
+          <div className='mb-3'>
+            <label className='mb-2 block'>Contraseña</label>
+            <input ref={passwordRef} type="password" className='input w-full'/>
+          </div>
+          <button type="submit" className='btn btn-primary' >
+            Aceptar
+          </button>
+
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default LoginView
